d3d179fd56f6b398dff354edcd3d2afcbd9b6556
untold-titan <untoldtitan3k@gmail.com>
2024-12-17 16:17:38 -0700

    Reformat index.jsx

--- app/index.jsx
+++ app/index.jsx
@@ -3,7 +3,6 @@ import { Link, Stack, useNavigation } from "expo-router";
 import { Header } from "@/components/header";
 
 export default function Index() {
-
   const nav = useNavigation();
 
   return (
@@ -13,62 +12,77 @@ export default function Index() {
           headerTitle: (props) => <Header data={props} />,
         }}
       />
-        <View
-          style={[
-            styles.container,
-            {
-              flexDirection: "column",
-            },
-            {
-              backgroundColor: "#F7F4F3"
-            }
-          ]}
-        >
-          {/* body */}
-          <View style={{ flex: 4, backgroundColor: "#F7F4F3", marginTop: 20 }}>
-            <Pressable style={styles.wotd} title="Word of the Day" onPress={() => {
+      <View
+        style={[
+          styles.container,
+          {
+            flexDirection: "column",
+          },
+          {
+            backgroundColor: "#F7F4F3",
+          },
+        ]}
+      >
+        {/* body */}
+        <View style={{ flex: 4, backgroundColor: "#F7F4F3", marginTop: 20 }}>
+          <Pressable
+            style={styles.wotd}
+            title="Word of the Day"
+            onPress={() => {
               // Pick random word from the list of words :)
-              nav.navigate("wordle",{
-                word:"CHIE",
+              nav.navigate("wordle", {
+                word: "CHIE",
                 guesses: 6,
-                theme: "Persona 4"
-              })}}><Text style={{fontSize: 40, textAlign: "center", fontWeight:"bold"}}>Word of the Day</Text></Pressable>
-            <Link href="/statistics" style={styles.link}>
-              Statistics
-            </Link>
-            <Text style={{fontSize: 25, textAlign: "center", marginVertical: 30}}>Themes:</Text>
-            <Link href="/word-length" style={styles.link}>
-              Cyberpunk 2077
-            </Link>
-            <Link href="/word-length" style={styles.link}>
-              Naruto
-            </Link>
-            <Link href="/word-length" style={styles.link}>
-              One Piece
-            </Link>
-            <Link href="/word-length" style={styles.link}>
-              Persona
-            </Link>
-            <Link href="/word-length" style={styles.link}>
-              Hunter x Hunter
-            </Link>
-            <Link href="/word-length" style={styles.link}>
-              Pokemon
-            </Link>
-            {/* <Link href="/wordle" style={styles.link}>
+                theme: "Persona 4",
+              });
+            }}
+          >
+            <Text
+              style={{ fontSize: 40, textAlign: "center", fontWeight: "bold" }}
+            >
+              Word of the Day
+            </Text>
+          </Pressable>
+          <Link href="/statistics" style={styles.link}>
+            Statistics
+          </Link>
+          <Text
+            style={{ fontSize: 25, textAlign: "center", marginVertical: 30 }}
+          >
+            Themes:
+          </Text>
+          <Link href="/word-length" style={styles.link}>
+            Cyberpunk 2077
+          </Link>
+          <Link href="/word-length" style={styles.link}>
+            Naruto
+          </Link>
+          <Link href="/word-length" style={styles.link}>
+            One Piece
+          </Link>
+          <Link href="/word-length" style={styles.link}>
+            Persona
+          </Link>
+          <Link href="/word-length" style={styles.link}>
+            Hunter x Hunter
+          </Link>
+          <Link href="/word-length" style={styles.link}>
+            Pokemon
+          </Link>
+          {/* <Link href="/wordle" style={styles.link}>
               Wordle Testing
             </Link> */}
-            {/* <Button title="Test New Word" onPress={() => {
+          {/* <Button title="Test New Word" onPress={() => {
               nav.navigate("wordle",{
                 word:"CHIE",
                 guesses: 6,
                 theme: "Persona 4"
               })
             }}/> */}
-          </View>
-          {/* footer */}
-          <View style={{ flex: 0.5, backgroundColor: "#F7F4F3" }} />
         </View>
+        {/* footer */}
+        <View style={{ flex: 0.5, backgroundColor: "#F7F4F3" }} />
+      </View>
     </>
   );
 }
@@ -77,7 +91,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
-  wotd:{
+  wotd: {
     fontSize: 40,
     padding: 10,
     borderWidth: 1,
@@ -85,7 +99,7 @@ const styles = StyleSheet.create({
     borderColor: "black",
     borderRadius: 20,
     textAlign: "center",
-    marginHorizontal: 30
+    marginHorizontal: 30,
   },
   link: {
     color: "black",
@@ -98,6 +112,6 @@ const styles = StyleSheet.create({
     borderRadius: 20,
     textAlign: "center",
     marginHorizontal: 40,
-    marginVertical: 5
+    marginVertical: 5,
   },
 });
